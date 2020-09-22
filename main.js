2ff6ebb209af47d0972e61f0b0f39663bd70ce93
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-09-22 13:51:23 -0400

    done

--- main.js
+++ main.js
@@ -2,9 +2,39 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(str) {
+  return exclaim = str + '!';
+}
 
+function getFirstCharacter(str) {
+  return str[0];
+}
 
+function getLastCharacter(str) {
+  return str[str.length-1]
+}
 
+function getOneCharacter(str, num) {
+  return str[num]
+}
+//getOneCharacter('Crystal', 2)
+
+function getTwoCharacters(str, num, num1) {
+  return str[num] + str[num1]
+}
+//getTwoCharacters('Crystal', 1, 3)
+
+function makeCapitalized(str) {
+  return str.toUpperCase()
+}
+
+function yellLouder(yell) {
+  return yell.toUpperCase() + '!!!'
+}
+
+function getInitials(init) {
+  return init[0] + '.' + init[init.indexOf(' ')+1] + '.';
+}
 
 
 /********************************************************************
